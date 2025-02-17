aa8fdfe24bb78b180c0eeb9b4160a3b83018e52c
Alejo Esco <alejoesa@gmail.com>
2025-02-17 14:55:45 -0500

    add cors

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Or specify your frontend's URL
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 console.log('Ejecutando ando');
 
 export default app;
